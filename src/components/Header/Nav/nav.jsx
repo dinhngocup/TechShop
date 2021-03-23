8fbfcd145d97d0d7bec4215a9f6647607278171d
Dinh Ngoc Uyen Phuong <dinhngocup@dinhngocup.local>
2021-03-23 21:58:37 +0700

    dropdown menu for product tab

--- src/components/Header/Nav/nav.jsx
+++ src/components/Header/Nav/nav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./_nav.scss";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import Search from "./search";
 function Nav(props) {
   return (
@@ -13,10 +13,33 @@ function Nav(props) {
               Home
             </NavLink>
           </li>
-          <li>
-            <NavLink activeClassName="active" to="/products">
+          <li className='dropdown-switcher'>
+            <NavLink
+              activeClassName="active"
+              to="/products"
+            >
               Products
             </NavLink>
+            <div className="dropdown">
+              <div className="row">
+                <div className="col">
+                  <Link to='/'>Smart Watch</Link>
+                  <Link to='/'>PC Accessories</Link>
+                  <Link to='/'>Audio System</Link>
+                  <Link to='/'>HeadPhone</Link>
+                </div>
+                <div className="col">
+                  <Link to='/'>Mouse</Link>
+                  <Link to='/'>Keyboard</Link>
+                  <Link to='/'>Gaming Desk/Chair</Link>
+                  <Link to='/'>Laptop </Link>
+                </div>
+                <div className="col">
+                  <Link to='/'>Monitor</Link>
+                  <Link to='/'>All</Link>
+                </div>
+              </div>
+            </div>
           </li>
           <li>
             <NavLink activeClassName="active" to="/news">
